2f60058c85b212ec7c62aec28207b823c96afb70
Pakorn Fudulyawajananont <pakornf@Pakorns-MacBook-Air-2.local>
2025-12-04 20:20:34 +0700

    update schema

--- backend/src/models/sale.js
+++ backend/src/models/sale.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
 
-const SaleSchema = new mongoose.Schema({
+const SaleSchema = new mongoose.Schema(
+  {
   sale_id: { type: Number, required: true, unique: true },
   customer_id: { type: Number, required: true },
   sale_datetime: { type: Date, required: true },
-  total_price: { type: Number, required: true }
-});
+  total_price: { type: Number, required: true },
+  prescription_id: { type: Number, required: true, default: null },
+  prescription_fulfillement_status: { type: String, enum: ["None", "Full", "Partial"], default: "None" },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Sale", SaleSchema);
