21d9ab47292423575f2e312222c6fda3ae2cf3c5
Pakorn Fudulyawajananont <pakornf@Pakorns-MacBook-Air-2.local>
2025-12-04 13:30:08 +0700

    fix bug

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,13 +3,14 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 
+import dashboardRoutes from "./routes/dashboardRoutes.js";
+
 import medicineRoutes from "./routes/medicineRoutes.js";
 import prescriptionRoutes from "./routes/prescriptionRoutes.js";
 import supplierRoutes from "./routes/supplierRoutes.js";
 import supplyOrderRoutes from "./routes/supplyOrderRoutes.js";
 import customerRoutes from "./routes/customerRoutes.js";
 import doctorRoutes from "./routes/doctorRoutes.js";
-import dashboardRoutes from "./routes/dashboardRoutes.js";
 import saleRoutes from "./routes/saleRoutes.js";
 
 dotenv.config();
@@ -23,13 +24,14 @@ app.use(express.json());
 connectDB();
 
 // Routes
+app.use("/dashboard", dashboardRoutes);
+
 app.use("/medicines", medicineRoutes);
 app.use("/prescriptions", prescriptionRoutes);
 app.use("/suppliers", supplierRoutes);
 app.use("/supply-orders", supplyOrderRoutes);
 app.use("/customers", customerRoutes);
 app.use("/doctors", doctorRoutes);
-app.use("/dashboard", dashboardRoutes);
 app.use("/sales", saleRoutes);
 
 app.get("/", (req, res) => {
